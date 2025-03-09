854ac497cfce98b2556c9452bc31be6e028d4040
Genesis Parra <itsme.geparra@gmail.com>
2025-03-09 15:18:39 +0100

    Variables y Tipos de Datos

---
+++ js/script.js
@@ -0,0 +1,30 @@
+console.log('Esta es la ruta de mi archivo script.js');
+
+console.log('Este es un mensaje desde mi archivo script.js');
+
+// Variables - Clase Práctica
+const nombreEstudiante = 'Genesis'; //String
+const edadEstudiante = 31; //Number
+const esArgentina = false; //Boolean
+
+// tipos de datos
+// String   - Cadena de texto
+// Puedo ocupar las siguientes comillas para declarar un string 
+let nombre = 'Genesis';   // Comillas simples
+let apellido = "Parra"; // Comillas dobles  
+let ciudad = `Santiago`;  // Comillas invertidas 
+
+//Boolean - Verdadero o Falso
+let esEstudiante = true;
+let esProfesor = false;
+
+// Null - Valor nulo
+let valorNulo = null;
+
+console.log(typeof nombre); 
+console.log(typeof apellido);
+console.log(typeof ciudad);
+console.log(typeof esEstudiante);
+console.log(typeof esProfesor);
+console.log(typeof valorNulo);
+
